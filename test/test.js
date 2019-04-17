428539c3f953fe0facdcf60ba24bab5e30a6a73b
ahuang352@gmail.com <ahuang352@gmail.com>
2019-04-17 14:46:31 -0400

    adding inital window test

--- test/test.js
+++ test/test.js
@@ -2,31 +2,43 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 
 const app = new Application({
   path: electronPath,
   args: [path.join(__dirname, "..")]
 });
-describe('Tests', function () {
-    this.timeout(10000)
-    beforeEach(function () {
-      return app.start()
-    })
 
-    afterEach(function () {
-    if (this.app && this.app.isRunning()) {
+global.before(function () {
+  chai.should();
+  chai.use(chaiAsPromised);
+});
+
+describe('Tests', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    return app.start()
+  })
+
+  afterEach(function () {
+    if (app && app.isRunning()) {
         return app.stop()
     }
   })
 
-    it('shows an initial window', () => {
-        const wc = app.client.getWindowCount()
-        console.log(wc)
-        //assert.deepStrictEqual(wc, 1, "Incorrect number of windows open on init. Make sure dev tools option is disabled.")
+  it('shows an initial window', () => {
+      // const wc = app.client.getWindowCount()
+      // console.log(wc)
+      // assert.deepStrictEqual(wc, 1, "Incorrect number of windows open on init. Make sure dev tools option is disabled.")
       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-            assert.equal(wc, 2)
+      //assert.equal(wc, 2)
 
-    });
+      return app.client.waitUntilWindowLoaded()
+        .getWindowCount().should.eventually.equal(1);
+
+  });
 
     // it('clear images brings up pop-up', () => {
     //     const clear = $('#clear_images')
