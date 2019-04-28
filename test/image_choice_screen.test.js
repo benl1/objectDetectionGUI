9a5c7643513ae2338ff368cb39588c5b0db73ea2
ahuang352@gmail.com <ahuang352@gmail.com>
2019-04-28 14:21:28 -0400

    adding cropping test and testing image

--- test/image_choice_screen.test.js
+++ test/image_choice_screen.test.js
@@ -7,9 +7,27 @@ import { ImageContainer, CroppingArea } from '../src/image_choice_screen/image_c
 import renderer from 'react-test-renderer';
 import App from '../src/control/app';
 describe('Image Choice screen', () => {
-    it('test that image choice renders', ()=>{
+    it('test that image choice renders correctly', ()=>{
         const component = renderer.create(
             <ImageChoiceScreen app={new App()} />,
+            document.getElementById('root')
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it('test that imagecontainer renders correctly', ()=>{
+        const component = renderer.create(
+            <ImageContainer app={new App()} />,
+            document.getElementById('root')
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it('tests that cropping area renders correctly', ()=>{
+        const component = renderer.create(
+            <CroppingArea app={new App()} />,
             document.getElementById('root')
         );
         let tree = component.toJSON();
@@ -22,7 +40,19 @@ describe('Image Choice screen', () => {
         component.instance().setState({showCroppingArea: true, last_img_path:""});
         component.instance().forceUpdate();
         component = component.update();
-        expect(component).toMatchSnapshot();
         expect(component.find(CroppingArea)).toHaveLength(1);
+    })
+
+    it('test cropping area functionality', ()=>{
+        // using mount so we can access the props
+        let parent = mount(<ImageContainer app={new App()} />);
+        let component = mount(<CroppingArea death = {parent.instance()} img_path = {"<rootDir>/assets/robot.jpg"}/>)
+        expect(component.prop('img_path')).toEqual("<rootDir>/assets/robot.jpg")
+        component.instance().handleWholeImage();
+
+        component.instance().forceUpdate();
+        component = component.update();
+        console.log(parent.prop('app'))
+        expect(parent.prop('app').images).toHaveLength(1);
     })
 });
